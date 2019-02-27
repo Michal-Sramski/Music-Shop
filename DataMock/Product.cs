05cb50733ea0570a19fdcea90cd317737a7a0cc2
Michał Śramski <michal.sramski@vp.pl>
2019-02-27 20:57:48 +0100

    Initial commit

---
+++ DataMock/Product.cs
@@ -0,0 +1,63 @@
+﻿using Sramski.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sramski.DataMock
+{
+    class Product : IProduct
+    {
+        protected static int iterator = 1;
+
+        public Product()
+        {
+            Id = Product.iterator++;
+        }
+        private Product(int id)
+        {
+            Id = id;
+        }
+        public Product(string name) : this()
+        {
+            Name = name;
+        }
+        public Product(string name, IProducer producer) : this(name)
+        {
+            Producer = producer;
+        }
+        public Product(string name, IProducer producer, double price) : this(name, producer)
+        {
+            Price = price;
+        }
+        public Product(string name, IProducer producer, double price, bool promotion) : this(name, producer, price)
+        {
+            Promotion = promotion;
+        }
+
+        public object Clone()
+        {
+            Product p = new Product(Id);
+            p.Name = Name;
+            p.Producer = Producer;
+            p.Price = Price;
+            p.Promotion = Promotion;
+            return p;
+        }
+
+        public void OverwriteValues(IProduct product)
+        {
+            Name = product.Name;
+            Producer = product.Producer;
+            Price = product.Price;
+            Promotion = product.Promotion;
+        }
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public IProducer Producer { get; set; }
+        public double Price { get; set; }
+        public bool Promotion { get; set; }
+    }
+}
---
+++ MusicShop/ViewModels/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Sramski.MusicShop.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute != null)
+            {
+                return canExecute(parameter);
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+    }
+
+}
